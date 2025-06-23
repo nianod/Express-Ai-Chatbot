65ad7825d93211dcfecf41b2b10be6ed7b1a7f7a
nianod <arnoldkk422@gmail.com>
2025-06-23 21:38:57 +0300

    backend set up

--- Server/index.js
+++ Server/index.js
@@ -1,9 +1,26 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
+
 const PORT = 3000
 
-app.get('/api', (req, res) => {
-    res.json({"users": [useInsertionEffect, PiUserSwitchDuotone, userThree]})
+
+app.use(cors())
+app.use(express.json())
+
+// Routes
+app.post('/chat', (req, res) => {
+    const { message } = req.body
+
+
+//Demo    
+    res.json({
+      reply: `you said: "${message}". Im just a simple bot`    
 })
 
-app.listen(PORT, () => {console.log("Server runnig")})
+})
+app.get('/', (req, res) => {
+    res.send("Chatbot is Live")
+})
+
+app.listen(PORT, () => {console.log(`Server runnig on port: ${PORT}`)})
