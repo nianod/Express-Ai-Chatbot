0470a90174d5ee69af438f9cb2fd79720bb1b8b9
nianod <arnoldkk422@gmail.com>
2025-06-23 16:13:37 +0300

    Ui design

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,8 @@
+import Ui from "./Model/Ui"
 
 function App() {
-
-
   return (
-    <>
-      <h2 className="text-red-600">Hello world</h2>
-    </>
+    <Ui />
   )
 }
 
--- src/Model/Ui.tsx
+++ src/Model/Ui.tsx
@@ -1,8 +1,25 @@
+import { useState } from "react"
+import { FaLocationArrow } from "react-icons/fa"
 
 const Ui = () => {
+
+    const [input, setInputValue] = useState("")
+
   return (
-    <div>
-      
+    <div className="max-w-md">
+      <h1 className="font-bold text-black text-2xl bg-green-700 justify-center flex">Chat Bot</h1>
+      <div className="flex items-center border justify-between w-90 p-3 rounded-2xl">
+        <input type="text" 
+        className="focus:outline-none w-70"
+          value={input}
+          onChange={(e) => setInputValue(e.target.value)}
+          placeholder="Ask anything..."
+        />
+        <button className="cursor-pointer"
+        type="submit"
+        >
+            <FaLocationArrow/></button>
+      </div>
     </div>
   )
 }
