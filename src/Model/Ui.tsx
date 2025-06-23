ecb6ae358492d5384dd37466244735e88a36ec99
nianod <arnoldkk422@gmail.com>
2025-06-23 16:34:34 +0300

    Ui design

--- src/Model/Ui.tsx
+++ src/Model/Ui.tsx
@@ -1,24 +1,44 @@
-import { useState } from "react"
+import { useState } from "react";
 import { FaLocationArrow } from "react-icons/fa"
 
 const Ui = () => {
+  const [input, setInputValue] = useState("")
 
-    const [input, setInputValue] = useState("")
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    if (input.trim() === "")
+     return
+    alert(`You asked: ${input}`)
+    setInputValue("")
+
+  }
 
   return (
-    <div className="max-w-md">
-      <h1 className="font-bold text-black text-2xl bg-green-700 justify-center flex">Chat Bot</h1>
-      <div className="flex items-center border justify-between w-90 p-3 rounded-2xl">
-        <input type="text" 
-        className="focus:outline-none w-70"
-          value={input}
-          onChange={(e) => setInputValue(e.target.value)}
-          placeholder="Ask anything..."
-        />
-        <button className="cursor-pointer"
-        type="submit"
+    <div className="border w-fit bg-pink-700 rounded-2xl m-auto mt-20 p-5">
+      <div className="max-w-md">
+        <h1 className="font-bold text-white text-2xl bg-green-700 w-full justify-center flex">
+          Chat Bot
+        </h1>
+        <form
+          className="flex items-center border justify-between w-80 p-3 rounded-2xl mt-30 mb-2"
+          onSubmit={handleSubmit}
         >
-            <FaLocationArrow/></button>
+          <input
+            type="text"
+            className="focus:outline-none w-64 bg-transparent text-white"
+            value={input}
+            onChange={(e) => setInputValue(e.target.value)}
+            placeholder="Ask anything..."
+          />
+          <button
+            className="cursor-pointer ml-2 text-white"
+            type="submit"
+            aria-label="Send message"
+            title="Send"
+          >
+            <FaLocationArrow />
+          </button>
+        </form>
       </div>
     </div>
   )
